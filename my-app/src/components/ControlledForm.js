83786633d14087424fa56b4dc74425f4a77ddc98
Samantha Atkinson <sammodeste@yahoo.com>
2025-04-18 17:56:57 +0100

    completed how to submit a form

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -5,25 +5,53 @@ export class ControlledForm extends Component {
       super(props)
     
       this.state = {
-         name: ""
+         name: "",
+        catergory: "website",
+        comments: ""
+
       }
     }
 
-handleNameChange = (event) => {
-//   const newValue = event.target.value;
- 
+handleChange = (event) => {
+  const { name, value } = event.target;
+  console.log(`This is the value of ${name}:`, event.target.value)
   this.setState({
-    name: event.target.value
+    [name]: value
+
   })
 } 
-   
+  
+handleSubmit = (event) => {
+  event.preventDefault();
+  console.log(this.state)
+}
 
   render() {
     return (
       <div>
-        <form>
+        <h2>Please fill out the form below:</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div>
          <label htmlFor="id-name">Your Name:</label>
-         <input onChange={this.handleNameChange}   value={this.state.name} id="id-name" name="name" type="text" />
+         <input  
+            value={this.state.name} 
+            onChange={this.handleChange}
+            id="id-name" name="name" type="text"
+             />
+             </div>
+             <div>
+          <label htmlFor="id-name">Query Category:</label>
+          <select value = {this.state.catergory} onClick={this.handleChange} id="id-catergory" name="catergory" type="text">
+            <option value="website">Website Issues</option>
+            <option value="order">Order Issues</option>
+            <option value="general">General Issues</option>
+          </select>
+          </div>
+          <div>
+          <label htmlFor="id-comments">Comments:</label>
+          <textarea value = {this.state.comments} onChange={this.handleChange} id="id-comments" name="comments" />
+          </div>
+         
          <input type="submit" value="Submit" />
         </form>
       </div>
