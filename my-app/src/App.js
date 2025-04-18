6d62ec63a8394ce9d14885b692eeb8194252554e
Samantha Atkinson <sammodeste@yahoo.com>
2025-04-18 21:53:37 +0100

    uncontrolled forms learning completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import ControlledForm from "./components/ControlledForm";
+import UncontrolledForm from "./components/UncontrolledForm";
 
 function App() {
   return (
     <div className="App">
-     <ControlledForm />
+     <UncontrolledForm />
   </div>
   );
 }
