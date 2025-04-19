aeefae7335da0f22482d16667b6715230fe2c7eb
Samantha Atkinson <sammodeste@yahoo.com>
2025-04-19 15:14:39 +0100

    Introduction to React Hooks completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import SearchBar from "./components/SearchBar";
+import ClassCounter from "./components/ClassCounter";
+import HooksCounter from "./components/HooksCounter";
 
 function App() {
   return (
     <div className="App">
-     <SearchBar />
+     <ClassCounter />
+     <HooksCounter />
   </div>
   );
 }
