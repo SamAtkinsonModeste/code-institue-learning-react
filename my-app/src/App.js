ff430d96d510ca5fd5f8900712a55b9987509cc4
Samantha Atkinson <sammodeste@yahoo.com>
2025-04-24 13:28:01 +0100

    Hook versions of get and post completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import  HTTPPost from "./components/HTTPPost";
+import  HTTPHooks from "./components/HTTPHooks";
 
 function App() {
   return (
     <div className="App">
-     <HTTPPost />
+     <HTTPHooks />
   </div>
   );
 }
