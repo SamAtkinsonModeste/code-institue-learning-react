ef324cb85d42e0166269d62635db47def9a6ada3
Samantha Atkinson <sammodeste@yahoo.com>
2025-04-24 08:52:08 +0100

    created request to api and displayed the data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import  UseEffectCounterOnce from "./components/UseEffectCounterOnce";
+import  HTTPRequests from "./components/HTTPRequests";
 
 function App() {
   return (
     <div className="App">
-     <UseEffectCounterOnce />
-  
+     <HTTPRequests />
   </div>
   );
 }
